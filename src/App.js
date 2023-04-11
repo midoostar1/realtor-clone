61f1da7a4d95aae444f5bb734a3f50377d8c72b5
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-04-11 03:21:08 -0500

    nav bar created with links to different pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 
 import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
+import Header from "./components/Header";
+
+
 
 
 
@@ -9,8 +17,16 @@ function App() {
   return (
   <>
   <Router>
+    <Header/>
     <Routes>
       <Route path="/" element = {<Home/>}  />
+      <Route path="/profile" element = {<Profile/>}  />
+      <Route path="/sign-in" element = {<SignIn/>}  />
+      <Route path="/offers" element = {<Offers/>}  />
+      <Route path="/sign-up" element = {<SignUp/>}  />
+      <Route path="/forgot-password" element = {<ForgotPassword/>}  />
+
+
     </Routes>
   </Router>
   </>
