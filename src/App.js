948fa508abdd1700f4c2ea79077c0669e18c73d5
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-04-10 23:41:14 -0500

    routes created for different pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
+import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import Home from "./pages/Home";
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-4xl'>hello world</h1>
-    </div>
+  <>
+  <Router>
+    <Routes>
+      <Route path="/" element = {<Home/>}  />
+    </Routes>
+  </Router>
+  </>
   );
 }
 
